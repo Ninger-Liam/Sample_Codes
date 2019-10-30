26f366ebe559441177c046c1fe59362f8d30db49
NIngerLiam <NIngerLiam@users.noreply.github.com>
2019-10-30 14:02:17 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- isbn.c
+++ isbn.c
@@ -4,11 +4,54 @@
 
 int main(void)
 {
-long isbn;
+    //declare the isbn variable
+    long isbn;
     
-do
-{
-    isbn = get_long("ISBN:");
-}
-while (isbn < 0);
+    //make sure the user inputs a positive integer
+    do
+    {
+        isbn = get_long("ISBN:");
+    }
+    while (isbn < 0);
+    
+    //declare each digit as a variable
+    int frist;
+    int second;
+    int third;
+    int fourth;
+    int fifth;
+    int sixth;
+    int seventh;
+    int eighth;
+    int ninth;
+    int tenth;
+    int x = 0;
+    int y = x;
+    //find the value of each digit
+    for(int digit = 10; digit > 0; digit--)
+    {
+        x = isbn % 10;
+        x = x * digit;
+        isbn = isbn / 10;
+        y = x + y;
+        printf("%i\n", x);
+        printf("  %i\n", y);
+    }
+    y = y / 11;
+    printf("%i\n", y);
+    if(y % 10 == 0)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    
+    //multiply each digit by which number digit it is
+    
+    //print yes or no depending on whether the ISBN is valid
+    
+    
+    
 }
